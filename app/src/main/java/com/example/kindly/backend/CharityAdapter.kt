aba877bb308ec9212949148c9e96f3043ed4b551
bimsara weerakoon <99312076+it21303302b@users.noreply.github.com>
2023-10-25 01:04:48 +0530

    Bimsara - getting the image

--- app/src/main/java/com/example/kindly/backend/CharityAdapter.kt
+++ app/src/main/java/com/example/kindly/backend/CharityAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.kindly.backend
 
-class CharityAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kindly.R
+
+class CharityAdapter(private var charityList: List<CharityDB>) :
+    RecyclerView.Adapter<CharityAdapter.CharityViewHolder>() {
+
+    inner class CharityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.tvCharityName)
+        val descriptionTextView: TextView = itemView.findViewById(R.id.tvDescription)
+        // Add other views if needed
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharityViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_charity, parent, false)
+        return CharityViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CharityViewHolder, position: Int) {
+        val currentItem = charityList[position]
+
+        holder.nameTextView.text = currentItem.name
+        holder.descriptionTextView.text = currentItem.description
+        // Bind other data as needed
+    }
+
+    override fun getItemCount(): Int {
+        return charityList.size
+    }
+
+    fun updateData(newData: List<CharityDB>) {
+        charityList = newData
+        notifyDataSetChanged()
+    }
 }
