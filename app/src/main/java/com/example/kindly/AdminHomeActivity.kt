0bc5d7939978992a3ab441e6e8c061297a4a82b2
it21250156 <it21250156@my.sliit.lk>
2023-10-24 14:25:08 +0530

    admin home

---
+++ app/src/main/java/com/example/kindly/AdminHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kindly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+    }
+}
