bd6e2723a9bba09d42e56dd582434caad237e396
it21250156 <it21250156@my.sliit.lk>
2023-10-25 15:03:11 +0530

    chat basics done

---
+++ app/src/main/java/com/example/kindly/backend/ChatAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.kindly.backend
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kindly.R
+
+class ChatAdapter(private val chatMessages: List<Chat>) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+    inner class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Define views in the chat item XML
+        val senderName: TextView = itemView.findViewById(R.id.textViewChatSender)
+        val messageContent: TextView = itemView.findViewById(R.id.textViewChatMessage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        // Inflate the chat item layout and create a ViewHolder
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.chat_item, parent, false)
+        return ChatViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        // Bind data to views in the ViewHolder
+        val chatMessage = chatMessages[position]
+        holder.senderName.text = chatMessage.sender
+        holder.messageContent.text = chatMessage.message
+    }
+
+    override fun getItemCount() = chatMessages.size
+}
